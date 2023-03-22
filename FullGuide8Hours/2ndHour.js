9de2e726b001870cc43da759c020e706df20bb3c
CodingWork23 <nickdemidenko32@gmail.com>
2023-03-22 17:56:37 +0100

    Update 2ndHour.js

--- FullGuide8Hours/2ndHour.js
+++ FullGuide8Hours/2ndHour.js
@@ -19,12 +19,98 @@ else{
 // checkbox
 
 document.getElementById('myButton').onclick = function() {
+
+    const myCheckbox = document.getElementById('myCheckbox');
+    const visaBtn = document.getElementById('visaBtn');
+    const mastercardBtn = document.getElementById('mastercardBtn');
+    const paypalBtn = document.getElementById('paypalBtn');
+    
+
     // checked --> wenn checkbox true ist
-    if(document.getElementById('myCheckbox').checked) {
+    if(myCheckbox.checked) {
         console.log('you are subscribed');
     }
     else {
         console.log('you are not subscribed');
     }
 
+    if(visaBtn.checked) {
+        console.log('You are paying with a Visa!');
+    }
+    else if(mastercardBtn.checked) {
+        console.log('You are paying with a Mastercard!');
+    }
+    else if(paypalBtn.checked) {
+        console.log('You are paying with a PayPal!');
+    }
+    else {
+        console.log('You are paying nothing')
+    }
+}
+
+
+
+
+
+
+// switch
+
+let grade = 'A';
+
+switch(grade) {
+    case 'A':
+        console.log('You did great!');
+        break;
+    case 'B':
+        console.log('You did good!');
+        break;
+    case 'C':
+        console.log('You did okay!');
+        break;
+    case 'D':
+        console.log('You passed ... barely!');
+        break;
+    case 'E':
+        console.log('You failed!');
+        break;
+    case 'F':
+        console.log('You failed!');
+        break;
+    default:
+        console.log(grade, 'is not a letter grade!');
+}
+/* Eine andere Alternative
+
+    switch(true) {
+    case grade >= 90:
+        console.log('You did great!');
+        break;
+    }
+
+*/
+
+
+
+
+
+
+// while loop
+
+let userName = '';
+/*
+do {
+    userName = window.prompt('Enter your name');
+}while(userName == '' || userName == null)
+console.log('Hello', userName);
+*/
+
+
+
+
+
+
+// for loop
+
+for(let counter = 1; counter <= 10; counter+=1) {
+    console.log(counter);
 }
