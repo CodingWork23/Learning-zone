ea8bb07d387bb7022dfc6d811e66a83f03fbef3b
CodingWork23 <nickdemidenko32@gmail.com>
2023-03-23 11:43:47 +0100

    2nd hour end

--- FullGuide8Hours/2ndHour.js
+++ FullGuide8Hours/2ndHour.js
@@ -110,7 +110,111 @@ console.log('Hello', userName);
 
 
 // for loop
-
+/*
 for(let counter = 1; counter <= 10; counter+=1) {
     console.log(counter);
 }
+*/
+
+// break ;  continue
+
+for(let i = 1; i<= 20; i+=1){
+    if (i == 19) {
+        break;      // unterbricht den Loop
+    }
+    if (i == 11 || i == 7) {
+        continue;   // überspringt die Runde
+    }
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+// loop in einem loop
+
+let symbol = '$';
+let rows = 5;
+let colums = 10;
+
+for (let i = 1; i <= rows; i+=1) {
+    for (let j = 1; j <= colums; j+=1) {
+        document.getElementById('myRectangle').innerHTML += symbol;
+        
+    }
+    document.getElementById('myRectangle').innerHTML += '<br>';
+}
+
+
+
+
+
+
+
+// condition ? exprIfTrue : exprFalse
+
+let adult = checkAge(21);
+console.log(adult);
+
+function checkAge(age) {
+
+    return age >= 18 ? true : false;
+}
+
+
+
+
+
+
+
+// andere Alternative für Strings mit Variabeln
+
+let username = 'Bro';
+let items = 3;
+let total = 75;
+
+// console.log('Hello', username);
+// console.log('You have', items, 'items in your card');
+// console.log('Your total is $', total);
+
+// console.log(`Hello ${username}`);
+// console.log(`You have ${items} items in your card`);
+// console.log(`Your total is $${total}`);
+
+let text =
+`Hello ${username}
+You have ${items} items in your card
+Your total is $${total}`;
+
+console.log(text);
+
+
+
+
+
+
+
+
+
+
+// Zahlen Eigenschaften
+
+let myNum = 123456.789;
+
+// myNum = myNum.toLocaleString('en-US'); // US Englisch
+// myNum = myNum.toLocaleString('hi-IN'); // Hindi
+// myNum = myNum.toLocaleString('de-DE'); // Deutsch
+
+// {style: 'currency', currency: 'Währung'} --> nationale Währung eingeben
+// myNum = myNum.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'});
+
+// myNum = myNum.toLocaleString(undefined, {style: 'percent'}); //Prozent%
+
+myNum = myNum.toLocaleString(undefined, {style: 'unit', unit: 'celsius'});
+
+
+console.log(myNum);
